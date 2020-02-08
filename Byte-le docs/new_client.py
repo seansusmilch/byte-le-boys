aacdd4c5345ccb161bb0b4376d6282d62b4219ca
erickbickler <erickbickler@gmail.com>
2020-02-08 13:02:28 -0600

    Merge remote-tracking branch 'remotes/origin/master'
    # Conflicts:
    #	Byte-le docs/new_client.py

--- Byte-le docs/new_client.py
+++ Byte-le docs/new_client.py
@@ -1,5 +1,7 @@
 from game.client.user_client import UserClient
 from game.common.enums import *
+import random as rand
+import operator
 
 
 class Client(UserClient):
@@ -22,6 +24,15 @@ class Client(UserClient):
             DisasterType.earthquake: DecreeType.rubber_boots,
             DisasterType.monster: DecreeType.fishing_hook,
             DisasterType.ufo: DecreeType.cheese,
+        }
+
+        self.sensor_to_disaster = {
+            SensorType.tornado: DisasterType.tornado,
+            SensorType.fire: DisasterType.fire,
+            SensorType.blizzard: DisasterType.blizzard,
+            SensorType.earthquake: DisasterType.earthquake,
+            SensorType.monster: DisasterType.monster,
+            SensorType.ufo: DisasterType.ufo,
         }
 
         self.decrees = [
@@ -50,9 +61,10 @@ class Client(UserClient):
     def take_turn(self, turn, actions, city, disasters):
         avail_effort = city.population
 
-        if city.structure < city.max_structure - 20:
-            actions.add_effort(ActionType.repair_structure, (city.max_structure - avail_effort) * 2)
-            avail_effort -= (city.max_structure - avail_effort) * 2
+        if city.structure < city.max_structure:
+            max_struct = city.max_structure
+            actions.add_effort(ActionType.repair_structure, (max_struct - avail_effort) * 2)
+            avail_effort -= (max_struct - avail_effort) * 2
             # add effort to repair city if structure below 50
 
         if city.population < city.structure:
@@ -71,39 +83,59 @@ class Client(UserClient):
             pass
 
         for i in range(len(lasting_disasters)):
-                actions.add_effort(lasting_disasters[i], lasting_disasters[i].effort_remaining)
-                if avail_effort > lasting_disasters[i].effort_remaining // 3 + 10:
+                if avail_effort > lasting_disasters[i].effort_remaining:
                     avail_effort -= avail_effort - lasting_disasters[i].effort_remaining
                 else:
                     avail_effort -= avail_effort
+                actions.add_effort(lasting_disasters[i], lasting_disasters[i].effort_remaining)
 
-        try:
-            lasting_disasters.sort(key=lambda x: lasting_disasters[0].effort_remaining)
-        except IndexError:
-            pass
+        avail_effort -= city.effort_remaining
+        actions.add_effort(ActionType.upgrade_city, city.effort_remaining)
+        if city.buildings[BuildingType.printer].level != BuildingLevel.level_one:
+            actions.add_effort(city.buildings[BuildingType.printer], max(city.gold, avail_effort))
 
-        actions.add_effort(city.buildings[BuildingType.billboard], avail_effort)
-        avail_effort -= avail_effort
+        if city.sensors[SensorType.ufo] != SensorLevel.level_three:
+            actions.add_effort(city.sensors[SensorType.ufo], city.sensors[SensorType.ufo].effort_remaining)
+        if city.sensors[SensorType.earthquake] != SensorLevel.level_three:
+            actions.add_effort(city.sensors[SensorType.earthquake], city.sensors[SensorType.earthquake].effort_remaining)
+        if city.sensors[SensorType.tornado] != SensorLevel.level_three:
+            actions.add_effort(city.sensors[SensorType.tornado], city.sensors[SensorType.tornado].effort_remaining)
 
-        print(
-            "blizz " + str(city.sensors[SensorType.blizzard].sensor_results) \
-            + "\nearth " + str(city.sensors[SensorType.earthquake].sensor_results) \
-            + "\nfire " + str(city.sensors[SensorType.fire].sensor_results) \
-            + "\nmonster " + str(city.sensors[SensorType.monster].sensor_results) \
-            + "\ntornado " + str(city.sensors[SensorType.tornado].sensor_results) \
-            + "\nufo " + str(city.sensors[SensorType.ufo].sensor_results)
-            )
-        print(str(self.previous_disaster) + "----------------")
 
-        if self.decree_lag <= 4:
-            if city.sensors[SensorType.earthquake].sensor_results >= .88:
-                self.decree_lag = 5
-                self.decree = self.disaster_to_decree[DisasterType.earthquake]
-            if city.sensors[SensorType.tornado].sensor_results >= .88:
-                self.decree_lag = 5
-                self.decree = self.disaster_to_decree[DisasterType.tornado]
-            if city.sensors[SensorType.ufo].sensor_results >= .88:
-                self.decree_lag = 5
-                self.decree = self.disaster_to_decree[DisasterType.ufo]
-        actions.set_decree(self.decree)
-        self.decree_lag -= 1
+        #print("Effort Remaining: " + str(avail_effort))
+
+
+        # print(
+        #     "blizz " + str(city.sensors[SensorType.blizzard].sensor_results) \
+        #     + "\nearth " + str(city.sensors[SensorType.earthquake].sensor_results) \
+        #     + "\nfire " + str(city.sensors[SensorType.fire].sensor_results) \
+        #     + "\nmonster " + str(city.sensors[SensorType.monster].sensor_results) \
+        #     + "\ntornado " + str(city.sensors[SensorType.tornado].sensor_results) \
+        #     + "\nufo " + str(city.sensors[SensorType.ufo].sensor_results)
+        #     )
+        # print(str(self.previous_disaster) + "----------------")
+
+        # if city.sensors[SensorType.earthquake].sensor_results >= .88:
+        #     self.decree_lag = 5
+        #     self.decree = self.disaster_to_decree[DisasterType.earthquake]
+        # if city.sensors[SensorType.tornado].sensor_results >= .88:
+        #     self.decree_lag = 5
+        #     self.decree = self.disaster_to_decree[DisasterType.tornado]
+        # if city.sensors[SensorType.ufo].sensor_results >= .88:
+        #     self.decree_lag = 5
+        #     self.decree = self.disaster_to_decree[DisasterType.ufo]
+        #
+        # # print("decree lag = " + str(self.decree_lag))
+        # if self.decree_lag <= 1:
+        #     actions.set_decree(self.decree)
+        #     # print("decree changed to " + self.decrees[self.decree])
+        # self.decree_lag -= 1
+
+        sensors = dict()
+        for sensor_type, sensor in city.sensors.items():
+            sensors[sensor_type] = sensor.sensor_results
+
+        decree = self.disaster_to_decree[self.sensor_to_disaster[max(sensors.keys(), key=lambda k: sensors[k])]]
+        actions.set_decree(decree)
+
+        # actions.set_decree(DecreeType.)
