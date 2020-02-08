d076f0431d32e6a827921b5bb4201173d1f9b734
erickbickler <erickbickler@gmail.com>
2020-02-07 20:56:57 -0600

    Co-authored-by: stummyhurt <seantsusmilch@gmail.com>

--- Byte-le docs/new_client.py
+++ Byte-le docs/new_client.py
@@ -2,11 +2,23 @@ from game.client.user_client import UserClient
 from game.common.enums import *
 
 
-Class Client(UserClient):
-    def __init__(sel):
+class Client(UserClient):
+    def __init__(self):
         super().__init__()
 
+        self.previous_disaster = None
 
+        self.lasting_disasters = [DisasterType.fire, DisasterType.blizzard, DisasterType.monster]
+
+        self.disaster_to_decree = {
+            DisasterType.fire: DecreeType.anti_fire_dogs,
+            DisasterType.tornado: DecreeType.paperweights,
+            DisasterType.blizzard: DecreeType.snow_shovels,
+            DisasterType.earthquake: DecreeType.rubber_boots,
+            DisasterType.monster: DecreeType.fishing_hook,
+            DisasterType.ufo: DecreeType.cheese,
+        }
+        
     def team_name(self):
         return 'Team 2'
 
@@ -17,6 +29,20 @@ Class Client(UserClient):
         return CityType.popular
 
     def take_turn(self, turn, actions, city, disasters):
-
+        avail_effort = city.population
+        
+        sens_type = 
+        sens_level =
+        
         for disaster in disasters:
-            if disaster.type in lasting_disasters:
+            
+
+        print(
+            "effort = " + avail_effort +
+            "\ngold = " + gold +
+            "\ndisaster type = " + disast_type +
+            "\ndisaster = " + disast +
+            "\nsens_type = " + sens_type + 
+            "\nsens_level = " + sens_level +
+            "\n"
+            )
